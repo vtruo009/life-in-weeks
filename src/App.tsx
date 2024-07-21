0270d1f81a90ad1fe0b3e64b3aad62acfcc8b48b
Van Truong <van.t414@gmail.com>
2024-07-20 18:40:47 -0700

    use context value

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import Calendar from "./components/Calendar"
-import SettingsProvider from "./contexts/SettingsContext";
+import Calendar from "./components/Calendar";
+import { SettingsProvider } from "./contexts/SettingsContext";
 
 function App() {
 	return (
