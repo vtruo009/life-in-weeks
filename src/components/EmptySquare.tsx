50abc5849f74c81e5c0334fef17c558a015d4bb0
Van Truong <van.t414@gmail.com>
2024-10-02 17:24:25 -0700

    fix popover

--- src/components/EmptySquare.tsx
+++ src/components/EmptySquare.tsx
@@ -4,26 +4,36 @@ import { Popover, ArrowContainer } from "react-tiny-popover";
 import Rating from "./Rating";
 
 function EmptySquare() {
-    const [isOpen, setIsOpen] = React.useState(false);
-    const [color, setColor] = React.useState('transparent');
-    const buttonRef = React.useRef<HTMLButtonElement>(null);
+	const [isOpen, setIsOpen] = React.useState(false);
+	const [color, setColor] = React.useState("transparent");
+	const buttonRef = React.useRef<HTMLButtonElement>(null);
 
-    return (
-        <Popover isOpen={isOpen} containerStyle={{ padding: '5px', top: '5px' }} onClickOutside={() => setIsOpen(false)} content={({ childRect, popoverRect }) =>
-            <ArrowContainer
-                position='top'
-                arrowSize={8}
-                arrowStyle={{ bottom: '5px' }}
-                style={{ width: '100px', display: 'flex' }}
-                arrowColor={'lightgray'}
-                childRect={childRect}
-                popoverRect={popoverRect}
-            >
-                <Rating compact OnClick={setColor} />
-            </ArrowContainer>}>
-            <Square color={color} OnClick={() => setIsOpen(!isOpen)} ref={buttonRef} />
-        </Popover>
-    )
+	return (
+		<Popover
+			isOpen={isOpen}
+			containerStyle={{ padding: "5px", top: "5px" }}
+			onClickOutside={() => setIsOpen(false)}
+			content={({ childRect, popoverRect }) => (
+				<ArrowContainer
+					position='top'
+					arrowSize={8}
+					arrowStyle={{ bottom: "5px" }}
+					style={{ width: "100px", display: "flex" }}
+					arrowColor={"lightgray"}
+					childRect={childRect}
+					popoverRect={popoverRect}
+				>
+					<Rating isCompact OnClick={setColor} />
+				</ArrowContainer>
+			)}
+		>
+			<Square
+				color={color}
+				OnClick={() => setIsOpen(!isOpen)}
+				ref={buttonRef}
+			/>
+		</Popover>
+	);
 }
 
 export default EmptySquare;
--- src/components/Rating.tsx
+++ src/components/Rating.tsx
@@ -4,42 +4,45 @@ import { COLOR_MAP, WEEK_RATING } from "../utils/mixins";
 import React from "react";
 
 const StyledColorLegend = styled.div<{ $compact: boolean }>`
-    width: 100%;
-    padding: 8px 3px;
-    display: flex;
-    flex-direction: row;
-    justify-content: space-around;
-    background-color: ${({ $compact }) => $compact ? "lightgray" : "transparent"};
+	width: 100%;
+	padding: 8px 3px;
+	display: flex;
+	flex-direction: row;
+	justify-content: space-around;
+	background-color: ${({ $compact }) =>
+		$compact ? "lightgray" : "transparent"};
 `;
 
 const StyledLabel = styled.div`
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    gap: 6px;
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	gap: 6px;
 `;
 
 interface RatingProps {
-    isCompact: boolean;
-    OnClick?: React.Dispatch<React.SetStateAction<string>>;
+	isCompact: boolean;
+	OnClick?: React.Dispatch<React.SetStateAction<string>>;
 }
 
 function Rating({ isCompact, OnClick: setColor }: RatingProps) {
-    return (
-        <StyledColorLegend $compact={isCompact}>
-            {Array.from(Object.values(WEEK_RATING), (rating) => (
-                <StyledLabel>
-                    <Square
-                        key={rating}
-                        color={COLOR_MAP[rating]}
-                        disabled={!isCompact}
-                        OnClick={() => { if (setColor) setColor(COLOR_MAP[rating]) }}
-                    />
-                    {!isCompact && <p>{rating}</p>}
-                </StyledLabel>
-            ))}
-        </StyledColorLegend>
-    );
+	return (
+		<StyledColorLegend $compact={isCompact}>
+			{Array.from(Object.values(WEEK_RATING), (rating) => (
+				<StyledLabel>
+					<Square
+						key={rating}
+						color={COLOR_MAP[rating]}
+						disabled={!isCompact}
+						OnClick={() => {
+							if (setColor) setColor(COLOR_MAP[rating]);
+						}}
+					/>
+					{!isCompact && <p>{rating}</p>}
+				</StyledLabel>
+			))}
+		</StyledColorLegend>
+	);
 }
 
 export default Rating;
