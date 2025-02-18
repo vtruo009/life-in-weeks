a04e9b1e9a3532ee666911003ac56374407ab2f2
Van Truong <van.t414@gmail.com>
2025-02-18 11:19:08 -0800

    restructure grid

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -27,13 +27,18 @@ const StyledRow = styled.div<{ $showRowCounter: boolean }>`
 	grid-template-columns: 10px 1fr;
 	grid-gap: 5px;
 	margin: 0px 5px;
+	align-items: end;
 
 	.age-count {
 		text-align: end;
-		width: 12px;
-		font-size: 12px;
+		width: 10px;
+		font-size: 10px;
 		margin: 0px 3px 6px 0px;
 		visibility: ${(props) => (props.$showRowCounter ? 'visible' : 'hidden')};
+	}
+
+	&.decade {
+		margin-bottom: 1rem;
 	}
 `;
 
@@ -47,7 +52,7 @@ function Calendar() {
 			<Rating compact={false} />
 			<StyledCalendarGrid>
 				{Array.from({ length: desiredAge }, (_, i) => (
-					<StyledRow $showRowCounter={i % 5 === 0}>
+					<StyledRow $showRowCounter={i % 5 === 0} className={`${i !== 0 && i % 10 === 0 ? 'decade' : ''}`}>
 						<p id='age-count' className='age-count'>
 							{i}
 						</p>
--- src/components/Year.tsx
+++ src/components/Year.tsx
@@ -15,26 +15,36 @@ const StyledColumn = styled.div<{ $showColumnCounter: boolean }>`
 	align-items: center;
 
 	.week-count {
-		font-size: 12px;
+		width: 10px;
+		text-align: center;
+		font-size: 10px;
 		margin: 0px 0px 3px 0px;
-		visibility: ${(props) => (props.$showColumnCounter ? 'visible' : 'hidden')};
+		display: ${(props) => (props.$showColumnCounter ? '' : 'none')};
+	}
+
+	&.mid-year {
+		column-gap: 1rem;
 	}
 `;
 
 function Year({ currentYear }: { currentYear: number }) {
 	return (
 		<StyledYear>
-			{Array.from({ length: NUM_WEEKS_IN_YEAR }).map((_, i) => (
+			{Array.from({ length: 52 }, (_, i) => (
 				<StyledColumn $showColumnCounter={i === 0 || (i + 1) % 5 === 0}>
 					{currentYear === 0 && (
 						<p id='week-count' className='week-count'>
 							{i + 1}
 						</p>
 					)}
+				</StyledColumn>
+			))}
+			{Array.from({ length: NUM_WEEKS_IN_YEAR }).map((_, i) => (
+				<StyledColumn $showColumnCounter={i === 0 || (i + 1) % 5 === 0} className={`${i === 25 ? 'mid-year' : ''}`}>
 					<EmptySquare key={i} currWeek={i} currYear={currentYear} />
 				</StyledColumn>
 			))}
-		</StyledYear>
+		</StyledYear >
 	);
 }
 
